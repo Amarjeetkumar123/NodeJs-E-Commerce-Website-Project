af6d800da8b7c13c83a3bba65673e7e9112c8f77
Amarjeet Kumar <amarjeetkumar802212@gmail.com>
2023-01-06 17:11:49 +0530

    UI modify

--- E-Commerce_Website_Project/schemas.js
+++ E-Commerce_Website_Project/schemas.js
@@ -1,14 +1,13 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 module.exports.productSchema = Joi.object({
-    name: Joi.string().required(),
-    img: Joi.string.require(),
-    price: Joi.number.require(),
-    desc: Joi.string.require()
+  name: Joi.string().required(),
+  img: Joi.string().required(),
+  price: Joi.number().min(0).required(),
+  desc: Joi.string().required(),
 });
 
-
 module.exports.reviewSchema = Joi.object({
-    rating: Joi.number().min(0).max(5),
-    comment: Joi.string().required()
+  rating: Joi.number().min(0).max(5),
+  comment: Joi.string().required(),
 });
